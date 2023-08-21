00de2e377b65752d352efb27a259cccfdcda69e0
hafiz <my081381350574@gmail.com>
2023-08-21 21:59:47 +0700

    - fix the environtment command on package json

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,25 @@
 import main from "./app";
 
+import dotenv from "dotenv";
+
+dotenv.config({
+  path:
+    process.env.NODE_ENV == "test"
+      ? ".env.test"
+      : process.env.NODE_ENV == "development"
+      ? ".env.local"
+      : ".env",
+});
+
+console.log(`Current environtmen : ${process.env.NODE_ENV}`);
 
 async function buildServer() {
-    const app = await main()
-    try {
-        await app.listen({ port: 3000 })
-    } catch (error) {
-        process.exit(1)
-    }
-
+  const app = await main();
+  try {
+    await app.listen({ port: 3000 });
+  } catch (error) {
+    process.exit(1);
+  }
 }
 
-buildServer()
+buildServer();
