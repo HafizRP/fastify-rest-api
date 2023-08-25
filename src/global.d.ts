7239b73437779cc20c7255da6ef3ebfcc16f95dd
HafizRP23 <hafiz.tradersfamily@gmail.com>
2023-08-25 18:52:14 +0700

    - add oauth login with google

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,21 +1,23 @@
 import { JWT } from "@fastify/jwt";
+import { OAuth2Namespace } from '@fastify/oauth2'
 
 declare module "fastify" {
-    interface FastifyRequest {
-      jwt: JWT;
-    }
-  
-    export interface FastifyInstance {
-      authenticate: any;
-    }
+  interface FastifyRequest {
+    jwt: JWT;
   }
-  
-  declare module "@fastify/jwt" {
-    interface FastifyJWT {
-      user: {
-        id: number;
-        name: string;
-        email: string;
-      };
-    }
+
+  export interface FastifyInstance {
+    authenticate: any;
+    googleOauth2: OAuth2Namespace
   }
+}
+
+declare module "@fastify/jwt" {
+  interface FastifyJWT {
+    user: {
+      id: number;
+      name: string;
+      email: string;
+    };
+  }
+}
