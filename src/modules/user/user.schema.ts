86ad38bb5b7330f17fb41e8cf29c60aebc8bdef2
HafizRP23 <hafiz.tradersfamily@gmail.com>
2023-09-14 14:47:09 +0700

    - add migration for add uuid on users table

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -30,7 +30,7 @@ const loginSchema = z.object({
       required_error: "Email is required",
       invalid_type_error: "Email must be a string",
     })
-    .email(),
+    .email({ message: "Email must be a valid email" }),
   password: z.string({
     required_error: "Password is required",
     invalid_type_error: "Password must be a string",
